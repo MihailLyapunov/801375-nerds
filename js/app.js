58efc6850e6870795e7a0870c2d4d285866cd495
Lyapunov Mihail <LyapunovMihail@gmail.com>
2018-07-08 23:02:26 +0300

    'finish_styling'

--- js/app.js
+++ js/app.js
@@ -15,14 +15,9 @@ function init () {
     let myPlacemark = new ymaps.Placemark([59.938631, 30.323055], {
         hintContent: 'ул. Б. Конюшенная, д. 19/8'
     }, {
-        // Необходимо указать данный тип макета.
         iconLayout: 'default#image',
-        // Своё изображение иконки метки.
         iconImageHref: 'img/bg/map-marker-png.png',
-        // Размеры метки.
         iconImageSize: [231, 190],
-        // Смещение левого верхнего угла иконки относительно
-        // её "ножки" (точки привязки).
         iconImageOffset: [-58, -193]
     });
     yandexMap.geoObjects.add(myPlacemark);
