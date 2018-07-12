a569d189bef0ed39f129e04f14a3930eb9910b75
Lyapunov Mihail <LyapunovMihail@gmail.com>
2018-07-12 04:26:43 +0300

    'fixes'

--- js/app.js
+++ js/app.js
@@ -1,5 +1,35 @@
-var yandexMap;
+'use strict';
 
+document.ready = (function() {
+    // Вешаем обработчик на открытие модального окна
+    let modalBtn = document.querySelector('.js-modal-target');
+    if (modalBtn) {
+        modalBtn.addEventListener('click', function(evt) {
+            if (evt) {
+                evt.preventDefault();
+            }
+            let modal = document.querySelector(`.js-modal`);
+            if (modal) {
+                modal.classList.add('modal-visible');
+                
+                // Вешаем обработчик закрытия модального окна
+                modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+            }
+        });       
+    }
+})();
+
+// Закрытия модального окна
+function closeModal() {
+    let modal = document.querySelector('.modal-visible');
+    if (modal) {
+        modal.classList.remove('modal-visible');
+        this.removeEventListener('click', closeModal);
+    }
+}
+
+
+var yandexMap;
 // Дождёмся загрузки API и готовности DOM.
 ymaps.ready(init);
 
@@ -9,6 +39,7 @@ function init () {
     yandexMap = new ymaps.Map('ya-map', {
         center: [59.938631, 30.323055],
         zoom: 15,
+        controls: ["zoomControl"]
     }, {
         searchControlProvider: 'yandex#search'
     });
@@ -21,4 +52,5 @@ function init () {
         iconImageOffset: [-58, -193]
     });
     yandexMap.geoObjects.add(myPlacemark);
+    yandexMap.behaviors.disable("scrollZoom");
 }
